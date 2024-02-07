440f5c88c7da932214255182fdf2582b12486153
HelloDraemer1234 <159123656+HelloDraemer1234@users.noreply.github.com>
2024-02-07 17:52:06 +0800

    Update weather_report1.py

--- weather_report1.py
+++ weather_report1.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 appID = os.environ.get("APP_ID")
 appSecret = os.environ.get("APP_SECRET")
 # 收信人ID即 用户列表中的微信号
-openId = os.environ.get("OPEN_ID")
+openId = os.environ.get("OPEN_ID1")
 # 天气预报模板ID
 weather_template_id = os.environ.get("TEMPLATE_ID")
 
@@ -130,4 +130,4 @@ def weather_report(this_city):
 
 
 if __name__ == '__main__':
-    weather_report("余干")
+    weather_report("南昌")
